7040d09ccd459501ceb516de43bcb772f22759a6
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-28 02:02:38 +0300

    apply MVC architecture

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 const ejs = require('ejs');
+const pageRoute = require('./routes/pageRoute');
+
+const app = express();
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -9,17 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+app.use('/', pageRoute);
 
 const port = 3000;
 app.listen(port, () => {
