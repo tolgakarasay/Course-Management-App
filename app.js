61d486a2217fe3a8d438ab2836450650355eec64
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-28 03:27:04 +0300

    create course functionality added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const ejs = require('ejs');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+// CONNECT TO DATABASE
+mongoose
+  .connect('mongodb://localhost/course-db')
+  .then(() => {
+    console.log('Database connection is successful.');
+  })
+  .catch((err) => {
+    console.log('Database connection failed.');
+    console.log(err);
+  });
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -12,6 +24,7 @@ app.use(express.static('public'));
 
 // ROUTES
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
