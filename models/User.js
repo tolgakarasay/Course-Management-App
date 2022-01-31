7454be6aa537b66ae8c66facc48fae65642befe6
Tolga Karasay <tolgakarasay@gmail.com>
2022-01-31 20:20:14 +0300

    add user roles

--- models/User.js
+++ models/User.js
@@ -19,6 +19,12 @@ const UserSchema = new Schema({
     required: true,
   },
 
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student',
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
